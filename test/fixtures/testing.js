c67803f4eb9a601400ea3b3a29a8a530b3d67934
Martin Rubinsztein <martin@brooklynpacket.com>
2014-08-30 01:40:02 -0300

    Adding unit test

--- test/fixtures/testing.js
+++ test/fixtures/testing.js
@@ -1,14 +1,19 @@
 /* Testing */
 
 //@define 4to8
+//@define 9and10
 var miArray = [
 //@include 123
 //@ifdef 4to8
 //@include ./scr/456.js
-,7,8
+, 7, 8
 //@ifndef skip9and10
-,9,10
+//@ifdef 9and10
+, 9, 10
+//@endif
 //@else
-"9'", "10'"
+//@ifdef 9and10
+, "9'", "10'"
+//@endif
 //@endif
 ];
